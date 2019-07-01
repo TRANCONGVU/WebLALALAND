f066807c0188ceee3fb42a32596117dd4d5c6022
vocongduc <voductrau@gmail.com>
2019-07-01 16:16:27 +0700

    css

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     //
     public function boot()
     {
-        // URL::forceScheme('https');
+        URL::forceScheme('https');
     }
 }
 // hello :)))
