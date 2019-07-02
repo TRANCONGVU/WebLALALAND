0799dae80e0e74904fc9fbf7928d770a72dfa6a8
kien chi dong <kienchidong@gmail.com>
2019-07-02 22:16:06 +0700

    up

--- app/Http/Controllers/Controller_1.php
+++ app/Http/Controllers/Controller_1.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
 
 class Controller_1 extends Controller
 {
@@ -59,5 +62,53 @@ class Controller_1 extends Controller
     }
     public  function get_dangnhap(){
     	return view('pages.dangnhap');
+    }
+
+    public function createuser(Request $request){
+
+        $this->validate($request,[
+            'email'		=>'unique:users,email',
+        ]);
+        $input= $request->all();
+        $birth = $input['year'].'-'.$input['month'].'-'.$input['day'];
+        if ($request->hasFile('avatar')) {
+            $file = $request->file('avatar');
+            $name = $file->getClientOriginalName();
+            $avatar = str_random(4) . "_avatar_" . $name;
+            while (file_exists('images/avatar/' . $avatar)) {
+                $Hinh = str_random(4) . "_avatar_" . $name;
+            }
+            $file->move('images/avatar/', $avatar);
+            $file_name = $avatar;
+        }
+        else{
+            if($input['gender']==0){
+                $file_name='male.png';
+            }
+            else{
+                $file_name='female.png';
+            }
+        }
+
+         DB::table('users')->insert([
+            'name' => $input['name'],
+            'birth' => $birth,
+            'avatar' => $file_name,
+            'gender' => $input['gender'],
+            'email' => $input['email'],
+            'password' => bcrypt($input['password_confirmation'])
+         ]);
+
+        if(Auth::guard('web')-> attempt(['email' => $request->email, 'password' => $request->password], $request -> remember)){
+
+            //nếu thành công thì chuyển hướng về view dashboard của admin
+            return redirect()-> intended(route('home'))->with('thongbao','Create account success');
+
+        }
+        else {
+
+            //thất bại
+            return redirect()->back()->withInput($req->only('email', 'remember'));
+        }
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,34 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
 
-        Schema::create('role', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-        });
-
-        Schema::create('admins', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name',191);
-            $table->string('email',191)->unique();
-            $table->string('password',191);
-            $table->timestamp('email_verified_at')->nullable();
-            $table->bigInteger('level')->unsigned();
-            $table
-                ->foreign('level')
-                ->references('id')
-                ->on('role');
-            $table->timestamps();
-        });
     }
 
     /**
