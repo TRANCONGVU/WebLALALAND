ed9fb34e1cfd7b8f0040feee509ad03d2f0fef1f
vocongduc <voductrau@gmail.com>
2019-08-09 09:09:19 +0700

    cac

--- resources/views/pages/gioithieu.blade.php
+++ resources/views/pages/gioithieu.blade.php
@@ -1,5 +1,9 @@
 @extends('master-layout')
 @section('content')
+<!-- Start WOWSlider.com HEAD section -->
+<link rel="stylesheet" type="text/css" href="{{asset('')}}hay/engine1/style.css" />
+<script type="text/javascript" src="{{asset('')}}hay/engine1/jquery.js"></script>
+<!-- End WOWSlider.com HEAD section -->
 <div class="container gioithieu d-flex flex-column">
     <h4 class="text-left text-uppercase">Giới thiệu</h4>
     <span>{!! $introduce->content !!}</span>
@@ -32,5 +36,21 @@
 
     </div>--}}
 </div>
-
+<!-- Start WOWSlider.com BODY section -->
+<div id="wowslider-container1">
+    <div class="ws_images"><ul>
+            <li><img src="{{asset('')}}hay/data1/images/dangcap.png" alt="dangcap" title="dangcap" id="wows1_0"/></li>
+            <li><a href="http://wowslider.com"><img src="data1/images/ducbeach.png" alt="jquery slider" title="ducbeach" id="wows1_1"/></a></li>
+            <li><img src="{{asset('')}}hay/data1/images/ducxe.png" alt="ducxe" title="ducxe" id="wows1_2"/></li>
+        </ul></div>
+        <div class="ws_bullets"><div>
+            <a href="#" title="dangcap"><span><img src="{{asset('')}}hay/data1/tooltips/dangcap.png" alt="dangcap"/>1</span></a>
+            <a href="#" title="ducbeach"><span><img src="{{asset('')}}hay/data1/tooltips/ducbeach.png" alt="ducbeach"/>2</span></a>
+            <a href="#" title="ducxe"><span><img src="{{asset('')}}hay/data1/tooltips/ducxe.png" alt="ducxe"/>3</span></a>
+        </div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.com">wowslideshow</a> by WOWSlider.com v8.5</div>
+    <div class="ws_shadow"></div>
+    </div>	
+    <script type="text/javascript" src="{{asset('')}}hay/engine1/wowslider.js"></script>
+    <script type="text/javascript" src="{{asset('')}}hay/engine1/script.js"></script>
+    <!-- End WOWSlider.com BODY section -->
 @endsection
